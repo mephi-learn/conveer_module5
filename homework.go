1dd4f336450d1143e7bb575ab257578c24cf1494
Vyacheslav Sitnikov <vsitnikov@gmail.com>
2024-11-26 20:37:09 +0300

    first commit

--- homework.go
+++ homework.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", homeWorkHandler)
-    http.ListenAndServe(":2024", nil)
+	http.HandleFunc("/", homeWorkHandler)
+	_ = http.ListenAndServe(":2024", nil)
 }
 
 func homeWorkHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Привет, я домашнее задание")
+	fmt.Fprintln(w, "Привет, я домашнее задание")
 }
